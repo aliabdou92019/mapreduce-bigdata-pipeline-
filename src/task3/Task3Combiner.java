caffcdb485c0afd433758190b94485603c2fcd5d
AliAbdou92019 <aliabdougithub@gmail.com>
2026-05-01 06:29:37 +0300

    Update Combiner

--- src/task3/Task3Combiner.java
+++ src/task3/Task3Combiner.java
@@ -3,25 +3,34 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class Task3Combiner extends Reducer<Text, CategoryStatsWritable, Text, CategoryStatsWritable> {
-
-    private final CategoryStatsWritable outputValue = new CategoryStatsWritable();
+public class Task3Combiner extends Reducer<Text, Text, Text, Text> {
 
     @Override
-    protected void reduce(Text key, Iterable<CategoryStatsWritable> values, Context context)
+    protected void reduce(Text key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
 
-        long totalRequestCount = 0L;
-        long totalResponseTimeSum = 0L;
-        long totalErrorCount = 0L;
+        long totalCount = 0;
+        long totalResponseTime = 0;
+        long totalErrors = 0;
 
-        for (CategoryStatsWritable value : values) {
-            totalRequestCount += value.getRequestCount();
-            totalResponseTimeSum += value.getResponseTimeSum();
-            totalErrorCount += value.getErrorCount();
+        for (Text value : values) {
+            String[] parts = value.toString().split("\\|");
+
+            if (parts.length < 3) {
+                continue;
+            }
+
+            try {
+                totalCount += Long.parseLong(parts[0]);
+                totalResponseTime += Long.parseLong(parts[1]);
+                totalErrors += Long.parseLong(parts[2]);
+            } catch (Exception e) {
+                continue;
+            }
         }
 
-        outputValue.set(totalRequestCount, totalResponseTimeSum, totalErrorCount);
-        context.write(key, outputValue);
+        context.write(
+                key,
+                new Text(totalCount + "|" + totalResponseTime + "|" + totalErrors));
     }
 }
